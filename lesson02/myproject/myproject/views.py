ad8eb2d028307f63cf51b1999b09762c32b2e433
Andre <andrepontde@gmail.com>
2025-01-24 17:23:27 +0000

    Lesson 2 Apps and templates

---
+++ lesson02/myproject/myproject/views.py
@@ -0,0 +1,12 @@
+# from django.http import HttpResponse
+from django.shortcuts import render
+
+def homepage(request):
+    # return HttpResponse("Hello World! I'm home")
+    return render(request, "home.html")
+
+def about(request):
+    # return HttpResponse("My About me page")
+    return render(request, "about.html")
+
+
